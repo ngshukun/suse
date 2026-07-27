197c4ee3aed552ef0716a96307b22c1e13e02369
ngshukun <ngshukun@gmail.com>
2026-07-27 11:05:22 +0800

    cloud init

---
+++ cloud-init.sh
@@ -0,0 +1,50 @@
+#cloud-config
+preserve_hostname: false
+fqdn: sles
+
+# Create the user and give sudo
+users:
+  - default
+  - name: suse
+    groups: [wheel]
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+
+# Set password (plaintext) – not recommended for production
+ssh_pwauth: true
+chpasswd:
+  expire: false
+  users:
+    - name: suse
+      password: "suse/4u"   # quotes avoid any YAML surprises
+      type: text
+
+# sshd is usually enabled already, but harmless:
+runcmd:
+  - systemctl enable --now sshd
+
+
+#cloud-config
+# Create the user and give sudo
+users:
+  - name: suse
+    groups: [wheel, adm, sudo]
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    lock_passwd: false
+
+# Set password (plaintext) – not recommended for production
+ssh_pwauth: true
+chpasswd:
+  expire: false
+  users:
+    - name: suse
+      password: "suse/4u"   # quotes avoid any YAML surprises
+      type: text
+
+# sshd is usually enabled already, but harmless:
+runcmd:
+  - systemctl enable --now sshd
+  - systemctl enable --now ssh
+  - [ systemctl, restart, ssh]
+  - [ systemctl, restart, sshd]
