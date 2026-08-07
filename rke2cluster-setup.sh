42589c06ca4886f48548be1c9cd7986748f018e4
ngshukun <ngshukun@gmail.com>
2026-08-07 15:01:08 +0800

    update steps

--- rke2cluster-setup.sh
+++ rke2cluster-setup.sh
@@ -1,2 +1,7 @@
 # to find out the secret for rancher prime check for the secret in cattle-system, bootstrap-secret
 kubectl get secret -n cattle-system bootstrap-secret -o go-template='{{.data.bootstrapPassword|base64decode}}'{{"\n"}}'
+
+# download rke2 script 
+# sudo as root
+
+curl -sfL http://get.rke2.io -o ./install.sh
